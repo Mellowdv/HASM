bc4b8d4480feece65d76461ba54ffd359531bf49
Mellowdv <tym.talik@gmail.com>
2022-06-26 13:24:05 +0200

    Assembler now takes a command line argument as per specification

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,19 @@
 #include "parser.h"
 #include "decoder.h"
 
-int main() {
+int main(int argc, char **argv) {
 
-    Parser parser("Pong.asm");
-    Decoder decoder("Pong.hack");
+    Parser parser(argv[1]);
+    std::string file {argv[1]};
+    std::string output_file {};
+    auto it = file.begin();
+    for (it; it < file.end(); it++) {
+        if (*it == '.') {
+            break;
+        }
+        output_file += *it;
+    }
+    Decoder decoder(output_file + ".hack");
     TokenStream ts;
     parser.parse(decoder, ts);
     return 0;
